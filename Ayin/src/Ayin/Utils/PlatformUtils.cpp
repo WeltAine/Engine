38e4490486bc8651474af7140abda500ab29b51a
WeltAine <1352410661@qq.com>
2026-05-16 15:14:10 +0800

    - FileDialog完善

--- Ayin/src/Ayin/Utils/PlatformUtils.cpp
+++ Ayin/src/Ayin/Utils/PlatformUtils.cpp
@@ -1,4 +1,4 @@
-#include <AyinPch.h>
+#include "AyinPch.h"
 
 #include "Ayin/Utils/PlatformUtils.h"
 
@@ -6,108 +6,97 @@
 #include "Ayin/Core/Window.h"
 
 #include <nfd.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
 #include <nfd_glfw3.h>
 
+#include <vector>
 
 namespace Ayin {
-
 	//nfdu8filteritem_t		文件过滤数据类型
 	//nfdopendialogu8args_t	打开的配置参数类型
 	//nfdsavedialogu8args_t	保存的配置参数类型
 
-	//static std::unique_ptr<::NFD::Guard> s_NfdGuard;	//没有这个啊，官方的readme中的示例也没有使用这个，为什么要这个？
 
-	void FileDialogs::Init() {
-		
+	void FileDialogs::Init()
+	{
 		NFD_Init();
-
 		//在调用 glfwInit() 和 NFD_Init() 之后、且打开任何文件对话框之前，
 		// 请调用一次以下函数，以告知 NFDe 您的应用程序所使用的 wl_display （如果您的应用程序未使用 Wayland，则此函数不会产生任何效果）：
 		NFD_SetDisplayPropertiesFromGLFW();
-		
-	};
-	void FileDialogs::Shutdown() {
+	}
 
+	void FileDialogs::Shutdown()
+	{
 		NFD_Quit();
-	
-	};
+	}
 
-	//打开文件对话框，返回选择的文件路径
-	std::string FileDialogs::OpenFile(std::initializer_list<FileFilter> filters, const char* defaultPath) {
-	
-		std::vector<nfdu8filteritem_t> ndfFilters;
-		ndfFilters.reserve(filters.size());
+	std::string FileDialogs::OpenFile(std::initializer_list<FileFilter> filters, const char* defaultPath)
+	{
 
-		for (FileFilter filter : filters) {
-			ndfFilters.emplace_back(filter.name, filter.filter);
+		std::vector<nfdu8filteritem_t> nfdFilters;
+		nfdFilters.reserve(filters.size());
+		for (const FileFilter& filter : filters) {
+			nfdFilters.push_back({filter.name, filter.spec});
 		}
 
-		nfdopendialogu8args_t openArg;
-		openArg.filterList = ndfFilters.data();
-		openArg.filterCount = ndfFilters.size();
+		//打开参数配置
+		nfdopendialogu8args_t openArg = {};
+		openArg.filterList = nfdFilters.data();
+		openArg.filterCount = static_cast<nfdfiltersize_t>(nfdFilters.size());
 		openArg.defaultPath = defaultPath;
-
 		//每次需要显示对话框时，请调用以下函数以获取父窗口句柄，并设置相应的参数
-		NFD_GetNativeWindowFromGLFWWindow(static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow()), &openArg.parentWindow);
+		NFD_GetNativeWindowFromGLFWWindow(
+			static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow()),
+			&openArg.parentWindow);
 
-		nfdu8char_t* outPath;
-		nfdresult_t result = NFD_OpenDialogU8_With(&outPath, &openArg);
-		if (result == NFD_OKAY)
-		{
-			NFD_FreePathU8(outPath);//?这是什么
-			return outPath;
-		}
-		else if (result == NFD_CANCEL)
-		{
-			AYIN_CORE_ASSERT(false, "User pressed cancel.");
-		}
-		else
-		{
-			AYIN_CORE_ASSERT(false, "Error: {0}", NFD_GetError());
-
-		}
-
-	};
-
-	//保存文件对话框，返回选择的文件路径
-	std::string FileDialogs::SaveFile(std::initializer_list<FileFilter> filters, const char* defaultName) {
-	
-		std::vector<nfdu8filteritem_t> ndfFilters;
-		ndfFilters.reserve(filters.size());
-
-		for (FileFilter filter : filters) {
-			ndfFilters.emplace_back(filter.name, filter.filter);
+		nfdu8char_t* outPath = nullptr;
+		switch (NFD_OpenDialogU8_With(&outPath, &openArg)) {
+			case NFD_OKAY: {
+				std::string result(outPath);
+				NFD_FreePathU8(outPath);
+				return result;
+			}
+			case NFD_CANCEL:
+				return {};
+			default:
+				AYIN_CORE_ERROR("File open error: {0}", NFD_GetError());
+				return {};
 		}
 
-		nfdsavedialogu8args_t saveArg;
-		saveArg.filterList = ndfFilters.data();
-		saveArg.filterCount = ndfFilters.size();
+	}
+
+	std::string FileDialogs::SaveFile(std::initializer_list<FileFilter> filters, const char* defaultName)
+	{
+
+		std::vector<nfdu8filteritem_t> nfdFilters;
+		nfdFilters.reserve(filters.size());
+		for (const FileFilter& filter : filters) {
+			nfdFilters.push_back({filter.name, filter.spec});
+		}
+
+		//保存参数配置
+		nfdsavedialogu8args_t saveArg = {};
+		saveArg.filterList = nfdFilters.data();
+		saveArg.filterCount = static_cast<nfdfiltersize_t>(nfdFilters.size());
 		saveArg.defaultName = defaultName;
-		//defaultPath不设置么
+		NFD_GetNativeWindowFromGLFWWindow(
+			static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow()),
+			&saveArg.parentWindow);
 
-		//两个default是拿来干嘛的？
-
-		//每次需要显示对话框时，请调用以下函数以获取父窗口句柄，并设置相应的参数
-		NFD_GetNativeWindowFromGLFWWindow(static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow()), &saveArg.parentWindow);
-
-		nfdu8char_t* outPath;
-		nfdresult_t result = NFD_SaveDialogU8_With(&outPath, &saveArg);
-		if (result == NFD_OKAY)
-		{
-			NFD_FreePathU8(outPath);//?这是什么
-			return outPath;
-		}
-		else if (result == NFD_CANCEL)
-		{
-			AYIN_CORE_ASSERT(false, "User pressed cancel.");
-		}
-		else
-		{
-			AYIN_CORE_ASSERT(false, "Error: {0}", NFD_GetError());
-
+		nfdu8char_t* outPath = nullptr;
+		switch (NFD_SaveDialogU8_With(&outPath, &saveArg)) {
+			case NFD_OKAY: {
+				std::string result(outPath);
+				NFD_FreePathU8(outPath);
+				return result;
+			}
+			case NFD_CANCEL:
+				return {};
+			default:
+				AYIN_CORE_ERROR("File save error: {0}", NFD_GetError());
+				return {};
 		}
 
-	};
-
+	}
 
 }
--- Ayin/src/Ayin/Utils/PlatformUtils.h
+++ Ayin/src/Ayin/Utils/PlatformUtils.h
@@ -9,8 +9,8 @@ namespace Ayin {
 
 	struct FileFilter {
 
-		const char* name;		//过滤器名称
-		const char* filter;		//过滤器字符串，例如"*.txt;*.docx"
+		const char* name;		// 过滤器友好名称，如 "场景文件"
+		const char* spec;		// 扩展名说明，逗号分隔，如 "hazel,yaml"（不要带点或通配符）
 
 	};
 
@@ -27,8 +27,8 @@ namespace Ayin {
 		//打开文件对话框，返回选择的文件路径
 		static std::string OpenFile(std::initializer_list<FileFilter> filters, const char* defaultPath);
 
-		//保存文件对话框，返回选择的文件路径
-		static std::string SaveFile(std::initializer_list<FileFilter> filters, const char* defaultPath);
+		//保存文件对话框，返回选择的保存路径
+		static std::string SaveFile(std::initializer_list<FileFilter> filters, const char* defaultName);
 
 	};
 
