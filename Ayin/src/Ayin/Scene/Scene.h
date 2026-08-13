6b9e0f3af97f11015c569b069637b1af704e581e
WeltAine <1352410661@qq.com>
2026-08-14 02:42:01 +0800

    - Scene 添加类 EnTT 的遍历接口
    # 实际上就是将遍历请求转发给 EnTT 的 each 方法。
    
    # 没有采用把 view 的数据包装成 vector<tuple<Entity, T&,.....>> 这样的，一方面是开销可能变大，二来这么干的目的核心还是遍历，那为什么不直接将遍历过程抛给 EnTT 呢，从包装 view 数据变成包装过程（转发）！

--- Ayin/src/Ayin/Scene/Scene.h
+++ Ayin/src/Ayin/Scene/Scene.h
@@ -97,6 +97,19 @@ namespace Ayin {
 		template<typename... ComponentTypes, typename... ExcludeComponentTypes>
 		std::vector<Entity> GetEntitiesByComponents(entt::exclude_t<ExcludeComponentTypes...> = entt::exclude_t<ExcludeComponentTypes...>{});
 
+
+		// 遍历操作元素
+		//! 如果你的一个类型参数是空类型，那么请不要在回调中添加该空类型参数，因为 EnTT 的空类型优化不会返回空类型的引用，空类型筛选还是没问题的，请放心（目前我不太想管这个问题，我现在甚至都要求使用者回调的第一个参数是 entt::entity 而不是 Entity，实际上在实现中我尝试了使用 lambda 再包装一次，不过空类型的问题还在就放弃了，所以这些问题就先抛给使用者了）
+		template<typename... ComponentTypes, typename Func, typename... ExcludeComponentTypes>
+			requires std::invocable<Func&, entt::entity, ComponentTypes&...>
+		void Each(Func&& func, entt::exclude_t<ExcludeComponentTypes...> = entt::exclude_t<ExcludeComponentTypes...>{});
+
+		// 遍历访问元素
+		template<typename... ComponentTypes, typename Func, typename... ExcludeComponentTypes>
+			requires std::invocable<Func&, entt::entity, const ComponentTypes&...>
+		void ConstEach(Func&& func, entt::exclude_t<ExcludeComponentTypes...> = entt::exclude_t<ExcludeComponentTypes...>{}) const;
+
+
 		Entity FindEntityByUUID(uint64_t UUID) const;
 
 
@@ -120,7 +133,7 @@ namespace Ayin {
 
 	template<typename... ComponentTypes, typename... ExcludeComponentTypes>
 	std::vector<Entity> Scene::GetEntitiesByComponents(entt::exclude_t<ExcludeComponentTypes...>) {
-	
+
 		auto&& view = m_Registry.view<ComponentTypes...>(entt::exclude_t<ExcludeComponentTypes...>{});
 
 		std::vector<Entity> entities;
@@ -132,6 +145,48 @@ namespace Ayin {
 		return entities;
 
 	};
+
+
+
+	template<typename... ComponentTypes, typename Func, typename... ExcludeComponentTypes>
+		requires std::invocable<Func&, entt::entity, ComponentTypes&...>
+		//! 一些成员函数对实例的值类别是有要求的，方法就是像 const 修饰一样，在同样的位置写 & 或者 && 就可以开启（我记得我前段时间还用过来着）
+		//! 而 std::invocable<Func& ...... 就是在强调以左值进行 invoke 检测（T& 必然是左值），之所以这么做是因为 view.each() 内部就是以左值进行调用
+	void Scene::Each(Func&& func, entt::exclude_t<ExcludeComponentTypes...>) {
+	
+		//auto lambda = [&func, this](entt::entity entity, ComponentTypes&... components) {
+		//	func(Entity{entity, this}, components...);
+		//	};
+
+
+		auto&& view = m_Registry.view<ComponentTypes...>(entt::exclude_t<ExcludeComponentTypes...>{});
+
+		view.each(std::forward<Func>(func));
+		//! std::forward 不是“模板函数里调用另一个函数时通常都要用”.
+		//! 它只适用于一种明确场景：当前函数是一个转发层，并且下层函数应该保留调用者传入参数的左值 / 右值属性。
+
+	};
+
+	template<typename... ComponentTypes, typename Func, typename... ExcludeComponentTypes>
+		requires std::invocable<Func&, entt::entity, const ComponentTypes&...>
+		//! 一些成员函数对实例的值类别是有要求的，方法就是像 const 修饰一样，在同样的位置写 & 或者 && 就可以开启（我记得我前段时间还用过来着）
+		//! 而 std::invocable<Func& ...... 就是在强调以左值进行 invoke 检测（T& 必然是左值），之所以这么做是因为 view.each() 内部就是以左值进行调用
+	void Scene::ConstEach(Func&& func, entt::exclude_t<ExcludeComponentTypes...>) const {
+	
+		//auto lambda = [&func, this](entt::entity entity, ComponentTypes&... components) {
+		//	func(Entity{entity, this}, components...);
+		//	};
+
+
+		auto&& view = m_Registry.view<ComponentTypes...>(entt::exclude_t<ExcludeComponentTypes...>{});
+
+		view.each(std::forward<Func>(func));
+		//! std::forward 不是“模板函数里调用另一个函数时通常都要用”.
+		//! 它只适用于一种明确场景：当前函数是一个转发层，并且下层函数应该保留调用者传入参数的左值 / 右值属性。
+
+
+	};
+
 
 
 }
