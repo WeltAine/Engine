4c293cabc2bd8389dae538ca8227363203c29b33
WeltAine <1352410661@qq.com>
2026-08-13 19:07:46 +0800

    -

--- Ayin/src/Ayin/Core/Timestep.h
+++ Ayin/src/Ayin/Core/Timestep.h
@@ -17,6 +17,7 @@ namespace Ayin {
 		/// Timestep -> float转换，默认转换为秒（目前的成员来说，它其实就是一个float）
 		/// </summary>
 		inline operator float() const { return m_TimeDuration; }//? 占用一次隐式转换么
+		//! 只能声明为类的非静态成员函数
 		//x 当然你可以选择更费劲的方法，全局域写两个operator +()，（写两个是为了对调参数，保证支持交换律，这也是不在Timestep中这么写的原因）
 
 		inline float GetSeconds() const { return m_TimeDuration; };
